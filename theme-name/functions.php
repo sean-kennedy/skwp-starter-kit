8dcaeaaaedf5297f8e03e1a02f1b5b5db949bab2
Sean Kennedy <skennedy@seans-imac.rowland.local>
2014-10-10 15:44:55 +1000

    Tweaked jQuery default loading position

--- theme-name/functions.php
+++ theme-name/functions.php
@@ -229,7 +229,7 @@
 	    if (!is_admin()) {
 	    
 	        wp_deregister_script('jquery');
-			wp_register_script('jquery', get_template_directory_uri() . '/js/jquery.min.js', array(), '1.11.1', true);
+			wp_register_script('jquery', get_template_directory_uri() . '/js/jquery.min.js', array(), '1.11.1', false);
 	        
 	        wp_register_script('menu', get_template_directory_uri() . '/js/menu.js', array('jquery'), '1.0.0', true);
 	        
