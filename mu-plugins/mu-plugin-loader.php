dadf6fa8fb14f9b087942dee796151ad9af41030
Sean Kennedy <skennedy@seans-imac.rowland.local>
2014-10-30 09:29:04 +1000

    Tweak to mu-plugin-loader.php

---
+++ mu-plugins/mu-plugin-loader.php
@@ -0,0 +1,15 @@
+<?php
+	
+/**
+ * Plugin Name: MU Plugin Loader
+ * Plugin URI: http://seankennedy.com.au/
+ * Description: Plugin loader for must use (MU) plugins: My Plugin
+ * Text Domain: mu-plugin-loader
+ * Author: Sean Kennedy
+ * Author URI: http://seankennedy.com.au/
+ * Version: 0.1.0
+ */
+
+require WPMU_PLUGIN_DIR . '/my-plugin/my-plugin.php';
+
+?>
