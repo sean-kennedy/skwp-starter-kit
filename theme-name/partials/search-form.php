483a9de1a54dbfb9bab78a6b815c928f6b44387a
Sean Kennedy <skennedy@seans-imac.rowland.local>
2015-05-13 15:30:01 +1000

    Tweaked search markup

--- theme-name/partials/search-form.php
+++ theme-name/partials/search-form.php
@@ -1,7 +1,7 @@
 <form method="get" class="search-form" action="<?php echo home_url(); ?>">
 
-	<input class="s" type="text" name="s" onfocus="if (this.value=='') { this.value=''; }" onblur="if (this.value=='') { this.value=''; }" value="">
+	<input class="search-input" type="text" name="s" onfocus="if (this.value=='') { this.value=''; }" onblur="if (this.value=='') { this.value=''; }" value="">
 	
-	<input class="searchsubmit" type="submit" value="Search">
+	<input class="search-submit" type="submit" value="Search">
 	
 </form>
