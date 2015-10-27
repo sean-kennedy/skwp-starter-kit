b5b7de470f6deb1f31319a4aeb228cf0cd379d6f
Sean Kennedy <sean@seankennedy.com.au>
2015-10-27 12:10:50 +1000

    Updated smoothstate

--- smoothstate-theme/js/scripts.js
+++ smoothstate-theme/js/scripts.js
@@ -2,65 +2,54 @@
 	
 	// SmoothState.js
     var $body = $('html, body'),
-    	relativeUrl;
     	$page = $('#main'),
-    smoothState = $page.smoothState({
-        prefetch: true,
-        pageCacheSize: 10,
-        // blacklist anything you dont want targeted
-        blacklist : '',
-        development : false,
-        // Runs when a link has been activated
-        onStart: {
-            duration: 0, // Duration of our animation
-            render: function (url, $container) {
-	            
-                // Add your CSS animation reversing class
-				$page.addClass('is-exiting');
-
-				// Restart your animation
-				smoothState.restartCSSAnimations();
-            }
-        },
-        onEnd: {
-            duration: 0, // Duration of the animations, if any.
-            render: function (url, $container, $content) {
-	            
-				$body.css('cursor', 'auto');
-                $body.find('a').css('cursor', 'auto');
-	            
-                $page.removeClass('is-exiting');
-                
-                $container.html($content);
-                
-                // Trigger document.ready and window.load
-                $(document).ready();
-                $(window).trigger('load');
-            }
-        },
-        callback: function(url, $container, $content) {
-	        
-	        relativeUrl = url.replace(location.protocol + '//' + location.hostname, '');
-	        
-	        // Trigger Google Analytics pageview
-			if (typeof window.ga !== 'undefined') {
-				ga('send', 'pageview', {
-				  'page': relativeUrl,
-				  'title': document.title
-				});
-			}
- 
-        }
-    }).data('smoothState');
+    	smoothStateOptions = {
+	        prefetch: true,
+	        blacklist: '.no-smoothState, .gform_button',
+	        cacheLength: 10,
+	        forms: 'form',
+	        debug: true,
+	        onStart: {
+	            duration: 400,
+	            render: function ($container) {
+		            
+	                // Add your CSS animation reversing class
+					$container.addClass('is-exiting');
 	
-	// Responsive Menu
-	$('.js-nav-list').responsiveMenu();
+					// Restart your animation
+					smoothState.restartCSSAnimations();
+					
+	            }
+	        },
+			onReady: {
+				duration: 0,
+				render: function ($container, $newContent) {
+					
+					// Remove your CSS animation reversing class
+					$container.removeClass('is-exiting');
 	
-	// iCheck
-	$('input').iCheck();
-	
-	// Heapbox
-	$('select').heapbox();
+					// Inject the new content
+					$container.html($newContent);
+					
+					// Trigger document.ready and window.load
+					$(document).ready();
+	                $(window).trigger('load');
+					
+	    		}	
+	    	},
+	        onAfter: function(url, $container, $content) {
+		        
+		        // Trigger Google Analytics pageview
+				if (typeof window.ga !== 'undefined') {
+					ga('send', 'pageview', {
+					  'page': smoothState.href.replace(location.protocol + '//' + location.hostname, ''),
+					  'title': document.title
+					});
+				}
+	 
+	        }
+	    },
+	    smoothState = $page.smoothState(smoothStateOptions).data('smoothState');
 
 })();
 
